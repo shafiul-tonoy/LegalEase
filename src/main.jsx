1a71e3245138086edbf8160c143bd9e6b605d05e
MD. Shafiul Anam <tonoy.tech@gmail.com>
2024-12-22 21:21:02 +0600

    tanstak query  added

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./routes/Router";
 import AuthProvider from "./providers/AuthProvider";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <AuthProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </AuthProvider>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,13 +1,40 @@
 import { Helmet } from "react-helmet-async";
+import { useQuery } from "@tanstack/react-query";
 
+import useAxios from "../hooks/useAxios";
+import Loading from "../components/Loading";
 
 export default function Services() {
-  
+  const axiosRequest = useAxios();
+
+  const fetchData = async () => {
+    const { data } = await axiosRequest.get("/services");
+    return data;
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["services"],
+    queryFn: fetchData,
+  });
+
+  if (isLoading) return <Loading />;
+  if (error)
+    return (
+      <div className="text-center text-red-500 mt-10">
+        <h2>Error Loading Services</h2>
+        <p>
+          We encountered an error while fetching services. Please try again
+          later.
+        </p>
+      </div>
+    );
+
   return (
     <>
       <Helmet>
         <title>LE | Services</title>
       </Helmet>
+      <div className="w-full md:w-10/12 mx-auto md:p-7">{data.length}</div>
     </>
   );
 }
